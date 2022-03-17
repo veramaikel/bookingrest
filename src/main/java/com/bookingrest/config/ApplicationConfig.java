4bc36100a609446b903003aad62ed4f10a80bcbf
Maikel Vera <vera.maikel@gmail.com>
2022-03-17 19:20:45 -0400

    New route to read data source connections

--- src/main/java/com/bookingrest/config/ApplicationConfig.java
+++ src/main/java/com/bookingrest/config/ApplicationConfig.java
@@ -1,15 +1,14 @@
 package com.bookingrest.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
+import org.springframework.core.io.Resource;
 
 import javax.sql.DataSource;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -19,25 +18,20 @@ public class ApplicationConfig {
     @Autowired
     private Environment env;
 
+    @Value("classpath:sql.properties")
+    private Resource sqlResource;
+
     @Bean
     public DataSource getDataSource() throws IOException {
-        FileInputStream propertiesInput = new FileInputStream("../sql.properties");
 
         Properties props = new Properties();
-        props.load(propertiesInput);
+        props.load(sqlResource.getInputStream());
 
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName(env.getProperty("spring.datasource.driver-class-name"));
         dataSourceBuilder.url((String)props.get("url"));
         dataSourceBuilder.username((String)props.get("username"));
         dataSourceBuilder.password((String)props.get("password"));
         return dataSourceBuilder.build();
-    }
-
-    @Bean
-    public PageableHandlerMethodArgumentResolverCustomizer pageableResolverCustomizer() {
-
-        return resolver -> resolver.setFallbackPageable(PageRequest.of(0,
-                Integer.parseInt(env.getProperty("spring.data.pageable.default-page-size"))));
     }
 }
